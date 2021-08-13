f73b1a2f7f82a0c2b81dc87f45d7c7de3b87ce3d
dhineshkathirvel <Dhinesh1607>
2021-08-13 22:10:56 +0530

    Adding Rows and Columns

--- lib/main.dart
+++ lib/main.dart
@@ -12,36 +12,45 @@ class MyApplication extends StatelessWidget {
         title: const Text('Login'),
         backgroundColor: Colors.deepOrangeAccent,
       ),
-      body: Container(
-        padding: EdgeInsets.all(90.0),
-        child: ElevatedButton.icon(
-            onPressed: (){
-              print("Login Successful !!");
-            },
-            icon: const Icon(
-                Icons.login,
-            ),
-            label: const Text(
-                "Click to login",
-            ),
-        ),
-        
-        //Image.asset('assets/images/Space.jpg'),
-        /*Text(
-            "Hello Dhinesh, Welcome !!",
-            style: TextStyle(
-                color: Colors.grey,
-                fontWeight: FontWeight.bold,
-                fontSize: 40.0,
-                fontFamily: 'StyleScript'
-            )
-        ),*/
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        child: const Text("Click"),
-        backgroundColor: Colors.deepOrangeAccent,
-      ),
+      body: Center(
+        child: Row(
+          children: [
+            Column(
+              children: [Text("User Name"),
+                Text("Password"),
+                ElevatedButton.icon(
+                  onPressed: (){},
+                  icon: Icon(Icons.login_rounded),
+                  label: Text("Sign In"),
+                )
+            ]),
+            Column(
+              children: [Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Enter a search term',
+                    ),
+                  ),
+                ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Enter a search term',
+                      ),
+                    ),
+                  ),
+                  ElevatedButton.icon(
+                    onPressed: (){},
+                    icon: Icon(Icons.login_rounded),
+                    label: Text("Cancel"),
+                  )
+                ]),
+            ],)
+          )
     );
   }
 }
